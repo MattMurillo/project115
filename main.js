19b3685c3b8bf29d678c49953f5376b78c866b5c
Error Studios <113478929+MattMurillo@users.noreply.github.com>
2023-04-01 14:39:11 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX = 0
+noseY = 0
+
+function preload()
+{
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
+}
+
+function setup()
+{
+canvas = createCanvas(300, 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log(" Nose X = " + results[0].pose.nose.x);
+        console.log(" Nose Y = " + results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+}
+
+function draw(){
+image(video, 0, 0, 300, 300)
+image(mustache, noseX -20, noseY -15, 50, 50);
+}
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
